b85a4c584830e26bb9c241e19516cc01a2f3c815
KieranMueller <kieran98mueller@gmail.com>
2023-08-01 13:40:58 -0500

    soft delete announcements

--- frontend/src/app/components/announcement-card/announcement-card.component.ts
+++ frontend/src/app/components/announcement-card/announcement-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
 @Component({
@@ -6,15 +7,31 @@ import { Input } from '@angular/core';
   templateUrl: './announcement-card.component.html',
   styleUrls: ['./announcement-card.component.css'],
 })
-export class AnnouncementCardComponent {
+export class AnnouncementCardComponent implements OnInit {
+  displayedDate = '';
+  isAdmin = false;
   @Input() name = '';
   @Input() content = '';
   @Input() date: Date = new Date();
   @Input() title = '';
+  @Input() id = 0;
 
-  displayedDate = '';
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.displayedDate = this.date.toDateString();
+    const user = JSON.parse(localStorage.getItem('user')!);
+    if (user) if (user.admin) this.isAdmin = true;
+  }
+
+  delete() {
+    return this.http
+      .put(`http://localhost:8080/announcements/delete/${this.id}`, null)
+      .subscribe({
+        error: (e) => console.log(e),
+        complete: () => {
+          window.location.reload();
+        },
+      });
   }
 }
